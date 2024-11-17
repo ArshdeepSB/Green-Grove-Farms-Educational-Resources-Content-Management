32bde91c5b8bcfdd77ad40e3ff810baeaac1d865
ArshdeepSB <37939492+ArshdeepSB@users.noreply.github.com>
2024-11-17 14:00:58 -0500

    Fix searchbar functionality

--- ercmanagement/src/App.js
+++ ercmanagement/src/App.js
@@ -7,28 +7,46 @@ import UserLogin from './pages/UserPage/index';
 import AdminResi from './pages/AdminResources/index2';
 import Login from './pages/Signin';
 import Signup from './pages/Signup';
-import EventsList from './pages/EventRegistration/Events'; // Import the EventsList component
+import EventsList from './pages/EventRegistration/Events';
 import SearchBar from './components/searchBar';
 import "./App.css"
 import UEventsList from './pages/userEvents';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState(''); // State to manage search term
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const AdminNavBar = () => (
+    <nav>
+      <Link to="/ResourcesLibrary">Resource Library</Link>
+      <Link to="/user-events">Events</Link>
+      <Link to="/AdminRes">Admin Resources</Link>
+      <Link to="/events">Admin Events</Link>
+      <Link to="/admin-login">Admin User</Link>
+      <button className="role-switch-btn" onClick={() => setIsAdmin(false)}>
+          Switch to User
+      </button>
+    </nav>
+  );
+
+  const UserNavBar = () => (
+    <nav>
+      <Link to="/ResourcesLibrary">Resource Library</Link>
+      <Link to="/user-events">Events</Link>
+      <Link to="/user-login">User </Link>
+      <button className="role-switch-btn" onClick={() => setIsAdmin(true)}>
+          Switch to Admin
+      </button>
+    </nav>
+  );
 
   return (
     <Router>
       <div>
-        <nav>
-          <Link to="/ResourcesLibrary">Resource Library</Link>
-          <Link to="/AdminRes">AdminRes</Link>
-          <Link to="/admin-login">Admin User</Link>
-          <Link to="/events">Admin Events</Link>
-          <Link to="/event-registration">Register For Events</Link>
-          <Link to="/user-login">User </Link>
-          <Link to="/user-events">Events</Link>
+        {isAdmin ? <AdminNavBar /> : <UserNavBar />}
+        {!isAdmin && (
           <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-        </nav>
-
+        )}
         <Routes>
           <Route path="/ResourcesLibrary" element={<ResourcesLibrary searchTerm={searchTerm} />}  />
           <Route path="/admin-login" component={<AdminLogin /> } />

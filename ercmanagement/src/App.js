24ec66f522210874e7677c82330ced4f46da974f
Mohammad Fardin Rahman <fardinr.24@gmail.com>
2024-11-17 22:30:16 -0500

    Changes to DB

--- ercmanagement/src/App.js
+++ ercmanagement/src/App.js
@@ -20,23 +20,13 @@ function App() {
       <div>
         <nav>
           <Link to="/ResourcesLibrary">Resource Library</Link>
-          <Link to="/AdminRes">AdminRes</Link>
-          <Link to="/admin-login">Admin User</Link>
           <Link to="/events">Admin Events</Link>
-          <Link to="/event-registration">Register For Events</Link>
-          <Link to="/user-login">User </Link>
           <Link to="/user-events">Events</Link>
           <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
         </nav>
 
         <Routes>
           <Route path="/ResourcesLibrary" element={<ResourcesLibrary searchTerm={searchTerm} />}  />
-          <Route path="/admin-login" component={<AdminLogin /> } />
-          <Route path="/user-login" component={<UserLogin />} />
-          <Route path="/AdminRes" element={<AdminRes />} />
-          <Route path="/AdminResi" element={<AdminResi />} />
-          <Route path="/login" element={<Login />}/>
-          <Route path="/signup" element={<Signup />}/>
           <Route path="/events" element={<EventsList />} />
           
 

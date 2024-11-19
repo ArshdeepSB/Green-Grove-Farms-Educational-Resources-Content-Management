f19627ba3357fb732b10bdce8650157ec047fcda
Mohammad Fardin Rahman <fardinr.24@gmail.com>
2024-11-19 17:54:17 -0500

    UI FIXED

--- ercmanagement/src/App.js
+++ ercmanagement/src/App.js
@@ -1,61 +1,48 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/common/navbar'; // Import Navbar
+import SearchBar from './components/searchBar';
 import ResourcesLibrary from './pages/ResourcePage/ResourcePage';
 import AdminRes from './pages/AdminResources';
-import AdminLogin from './pages/AdminPage/index';
-import UserLogin from './pages/UserPage/index';
-import AdminResi from './pages/AdminResources/index2';
-import Login from './pages/Signin';
-import Signup from './pages/Signup';
 import EventsList from './pages/EventRegistration/Events';
-import SearchBar from './components/searchBar';
-import "./App.css"
 import UEventsList from './pages/userEvents';
+import HomePage from './pages/HomePage';
+import './App.css';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [isAdmin, setIsAdmin] = useState(false);
 
-  const AdminNavBar = () => (
-    <nav>
-      <Link to="/ResourcesLibrary">Resource Library</Link>
-      <Link to="/user-events">Events</Link>
-      <Link to="/AdminRes">Admin Resources</Link>
-      <Link to="/events">Admin Events</Link>
-      <Link to="/admin-login">Admin User</Link>
-      <button className="role-switch-btn" onClick={() => setIsAdmin(false)}>
-          Admin
-      </button>
-    </nav>
-  );
-
-  const UserNavBar = () => (
-    <nav>
-      <Link to="/ResourcesLibrary">Resource Library</Link>
-      <Link to="/user-events">Events</Link>
-      <Link to="/user-login">User </Link>
-      <button className="role-switch-btn" onClick={() => setIsAdmin(true)}>
-          User
-      </button>
-    </nav>
-  );
+  const handleRoleSwitch = () => setIsAdmin(!isAdmin);
 
   return (
     <Router>
       <div>
-        {isAdmin ? <AdminNavBar /> : <UserNavBar />}
-        {!isAdmin && (
-          <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-        )}
+        {/* Navbar Component */}
+        <Navbar className="Navbar" isAdmin={isAdmin} onRoleSwitch={handleRoleSwitch} />
+        <br></br>
+        <br></br>
+
+        <br></br>
+        
+
+      
+
+       
+
+     
+
+
+        {/* SearchBar for users */}
+        {!isAdmin && <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />}
+        
+        {/* Routes */}
         <Routes>
-          <Route path="/" element={<ResourcesLibrary searchTerm={searchTerm} />} />
-          <Route path="/ResourcesLibrary" element={<ResourcesLibrary searchTerm={searchTerm} />}  />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/ResourcesLibrary" element={<ResourcesLibrary searchTerm={searchTerm} />} />
           <Route path="/events" element={<EventsList />} />
           <Route path="/AdminRes" element={<AdminRes />} />
-
-          <Route path="/user-events" element={< UEventsList searchTerm={searchTerm}/>} />
-
-
+          <Route path="/user-events" element={<UEventsList searchTerm={searchTerm} />} />
         </Routes>
       </div>
     </Router>
---
+++ ercmanagement/src/pages/HomePage/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "../../components/common/Button";
+import './styles.css';
+import { Link } from "react-router-dom";
+
+
+
+
+
+function HomePage() {
+    return (
+      <div className='hero-container'>
+        <video src='/videos/HomeVid.mp4' autoPlay loop muted />
+        <h1>Green Farms Educational Platform.</h1>
+        <p>Empowering dreams, securing futures: Investing in you, Investing with you.</p>
+        <div className='hero-btns'>
+            <Link to='/ResourcesLibrary'>
+          <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'
+          >
+            GET STARTED
+          </Button>
+          </Link>
+          
+        </div>
+      </div>
+      
+    );
+  }
+  
+  export default HomePage;

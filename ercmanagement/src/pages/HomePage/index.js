5563da440fe2e9c4620b41cc527aa12f62109e95
Mohammad Fardin Rahman <fardinr.24@gmail.com>
2024-11-19 18:53:01 -0500

    Tagline Update

--- ercmanagement/src/pages/HomePage/index.js
+++ ercmanagement/src/pages/HomePage/index.js
@@ -12,7 +12,7 @@ function HomePage() {
       <div className='hero-container'>
         <video src='/videos/HomeVid.mp4' autoPlay loop muted />
         <h1>Green Farms Educational Platform.</h1>
-        <p>Empowering dreams, securing futures: Investing in you, Investing with you.</p>
+        <p>Growing Knowledge, Cultivating Success: Your Gateway to Smarter Farming.</p>
         <div className='hero-btns'>
             <Link to='/ResourcesLibrary'>
           <Button
